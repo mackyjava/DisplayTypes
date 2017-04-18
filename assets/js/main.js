38d5ce35c90c3675e0367a267840fcc9d03cd278
mackyjava <adriana.jauregui.varela@gmail.com>
2017-04-18 11:58:52 -0500

    enlace javascript y funciones ocultar y restaurar imagenes

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+var cerrar = document.getElementsByClassName("cerrar");
+
+for(var i = 0; i<cerrar.length; i++){
+ cerrar[i].addEventListener("click",cerrarImagen)
+}
+
+function cerrarImagen(){
+ var div= this.parentElement;
+ div.style.display = "none";
+}
+
+var btnRestaurar = document.getElementById("btnRestaurar");
+btnRestaurar.addEventListener("click", restaurarImagenes);
+
+function restaurarImagenes(){
+ for(var i = 0; i<cerrar.length; i++){
+   cerrar[i].parentElement.style.display = "inline-block";
+ }
+}
